2aeb787e565fa858bfc40af0c9b6985881a4b55f
12210024 <12210024.gcit@rub.edu.bt>
2023-03-31 04:15:14 +0000

    updates

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express")
 const app = express()
 
 module.exports = app
+const userRouter = require('./routes/userrRoutes')
+
+app.use(express.json())
+
+app.use('/api/v1/users',userRouter)
 // const port = 4001
 // app.listen(port, () => {
 //     console.log('App runnig on port ${port} ..')
