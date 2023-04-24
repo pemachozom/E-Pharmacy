35d561876621cd726f86165b8d25e933e5e9276f
12210024 <12210024.gcit@rub.edu.bt>
2023-04-24 14:52:27 +0000

    updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,73 +1,92 @@
 const User = require('./../models/userModels')
-const jwt = require('jsonwebtoken')
-const AppError =require('./../utils/appError')
+const jwt = require ('jsonwebtoken')
+const AppError = require('./../utils/appError')
 
 const signToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,   
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn :  process.env.JWT_EXPIRES_IN,
     })
 }
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
     const cookieOptions = {
-        // expires : new Date (
-        //     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN  * 24 * 60 * 60 * 1000,
-        // ),
-        httpOnly: true,
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 *1000,
+        ), 
+        httpOnly: true, 
     }
 
     res.cookie('jwt', token, cookieOptions)
-
     res.status(statusCode).json({
-        status : "success",
+        status : 'success',
         token,
         data : {
             user
         }
     })
-}
+    
 
+}
 exports.signup = async (req, res, next) => {
     try{
     const newUser = await User.create(req.body)
-    createSendToken(newUser, 201, res)
 
-    const token =signToken(newUser._id)
+    createSendToken(newUser, 201, res)
+    const token = signToken(newUser._id)
+
     res.status(201).json({
-        status: 'success',
+        status : 'success',
         token,
-        data:{
-            user:newUser
+        data : {
+            user : newUser
         }
-    })}
+    }
+    
+    )}
+    
+
     catch(err) {
         res.status(500).json({error: err.message});
+
     }
 }
 
 exports.login = async (req, res, next) => {
     try{
-        const {email, password} = req.body
+        const { email, password} = req.body
 
-        if (!email || !password){
-            return next(new AppError('Please provide an email and password!',400))
+        if( !email || !password ){
+            return next(new AppError('Please provide an email and password!', 400))
         }
-        const user = await User.findOne({email}).select('+password')
         
 
-        if (!user || !(await user.correctPassword(password, user.password))) {
-            return next(new AppError('Incorrect email or password',401))
-        }
-        // const token = signToken(user._id)
-        // res.status(200).json({
-        //     status : 'success',
-        //     token,
-        // })
+        const user = await User.findOne({email}).select('+password')
 
+        const correct = await user.correctPassword(password, user.password)
+
+        if (!user || !correct) {
+            return next(new AppError('Incorrect email and password!', 401))
+        }
+
+        // if (!user || ! (await user.correctPassword(password, user.password))) {
+        //     return next(new AppError('Incorrect email and password!', 401))
+ 
+        // }
+      
         createSendToken(user, 200, res)
 
-    } catch (err) {
-        res.status(500).json({error : err.message});
+        const token = signToken(user._id)
+        res.status(200).json({
+            status : 'success',
+            token
+        })
+            
+            
+    }
+
+    catch(err) {
+        res.status(500).json({error: err.message});
+
     }
 }
